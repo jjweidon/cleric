487f5f5409ac1dc0e9f6f8ccca2879ca10103a4a
정재웅 <jweidon0821@gmail.com>
2025-04-14 19:17:21 +0900

    init(server): 서버 프로젝트 세팅

---
+++ server/src/main/java/com/insightnexus/cleric/ClericApplication.java
@@ -0,0 +1,13 @@
+package com.insightnexus.cleric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClericApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClericApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/insightnexus/cleric/ClericApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insightnexus.cleric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClericApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
